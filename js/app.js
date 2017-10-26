6c20318d2178d2449d58fac78a3f8dd2cca2801d
Carmen Perezchica <crperezchica@gmail.com>
2017-10-26 16:58:37 -0700

    Q:6 fixed for loop and if states

--- js/app.js
+++ js/app.js
@@ -4,76 +4,97 @@ alert('Hello there!');
 
 //Question one
 
-runSister();
+// runSister();
 
-function runSister() {
-    const sister = true;
-    const sisterAns = prompt('First off, do I have a sister?');
-    console.log('has a sister? ' + sister);
-    if (sisterAns.toLowerCase() === 'yes') {
-        alert('You are correct! Her name is Trevi and she lives in Washington.');
-    } else {
-        alert('I do have a sister. Her name is Trevi and she lives in Washington.');
-    }
-};
+// function runSister() {
+//     const sister = true;
+//     const sisterAns = prompt('First off, do I have a sister?');
+//     console.log('has a sister? ' + sister);
+//     if (sisterAns.toLowerCase() === 'yes') {
+//         alert('You are correct! Her name is Trevi and she lives in Washington.');
+//     } else {
+//         alert('I do have a sister. Her name is Trevi and she lives in Washington.');
+//     }
+// };
 
-//Question 2 = false
+// //Question 2 = false
 
-runStudies();
+// runStudies();
 
-function runStudies () {
-    const studies = ['Graphic Design', 'Linguistics', 'JavaScript'];
-    const studiesAns = prompt('Have I studied Astronomy?');
-    console.log('studied Astronomy? ' + studies.includes('Astronomy'));
-    if (studiesAns.toLowerCase() === 'no') {
-        alert('You are correct! I have studied a few things, but Astronomy is not one of them.');
-    } else {
-        alert('Alas, I have not studied Astronomy. I did study Linguistics though.');
-    }
-};
+// function runStudies () {
+//     const studies = ['Graphic Design', 'Linguistics', 'JavaScript'];
+//     const studiesAns = prompt('Have I studied Astronomy?');
+//     console.log('studied Astronomy? ' + studies.includes('Astronomy'));
+//     if (studiesAns.toLowerCase() === 'no') {
+//         alert('You are correct! I have studied a few things, but Astronomy is not one of them.');
+//     } else {
+//         alert('Alas, I have not studied Astronomy. I did study Linguistics though.');
+//     }
+// };
 
-//Question 3 = true
+// //Question 3 = true
 
-runAnimals();
+// runAnimals();
 
-function runAnimals() {
-    const animals = ['cats', 'dogs', 'giraffes'];
-    const animalsAns = prompt('Do I like giraffes?');
-    console.log('likes giraffes? ' + animals.includes('giraffes'));
-    if (animalsAns.toLowerCase() === 'yes') {
-        alert('It\'s true, I do. I have never met one but they seem like gentle creatures.');
-    } else {
-        alert('I do like giraffes. They seem like gentle creatures.');
-    }
-};
+// function runAnimals() {
+//     const animals = ['cats', 'dogs', 'giraffes'];
+//     const animalsAns = prompt('Do I like giraffes?');
+//     console.log('likes giraffes? ' + animals.includes('giraffes'));
+//     if (animalsAns.toLowerCase() === 'yes') {
+//         alert('It\'s true, I do. I have never met one but they seem like gentle creatures.');
+//     } else {
+//         alert('I do like giraffes. They seem like gentle creatures.');
+//     }
+// };
 
-//Question 4 = true
+// //Question 4 = true
 
-runActivities();
+// runActivities();
 
-function runActivities () {
-    const activities = ['fishing', 'hiking', 'JavaScript'];
-    const activitiesAns = prompt('Do I like to fish?');
-    console.log('likes to fish? ' + activities.includes('fishing') );
-    if (activitiesAns.toLowerCase() === 'yes') {
-        alert('Yep, I do! I fish on the Missouri River with my Dad every year.');
-    } else {
-        alert('Actually, walleye fishing is one of my favorite things to do.');
-    }
-};
+// function runActivities () {
+//     const activities = ['fishing', 'hiking', 'JavaScript'];
+//     const activitiesAns = prompt('Do I like to fish?');
+//     console.log('likes to fish? ' + activities.includes('fishing') );
+//     if (activitiesAns.toLowerCase() === 'yes') {
+//         alert('Yep, I do! I fish on the Missouri River with my Dad every year.');
+//     } else {
+//         alert('Actually, walleye fishing is one of my favorite things to do.');
+//     }
+// };
 
+// //Question 5 = false
 
-// const snacks = ['salami', 'cheese', 'gummy bears'];
-// const snacksAns = prompt('Do I eat spaghetti?');
-// console.log('eats spaghetti? ' + snacks.includes('spaghetti'));
-// if (snacksAns.toLowerCase() === 'no') {
-//     alert('Correct, I do not care for spaghetti. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. But if I were in Italy I would totally eat pasta.');
-// } else {
-//     alert('No spaghetti for me. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. But if I were in Italy I would totally eat pasta.');
-// }
+// runSnacks();
+
+// function runSnacks () {
+//     const snacks = ['salami', 'cheese', 'gummy bears'];
+//     const snacksAns = prompt('Do I eat spaghetti?');
+//     console.log('eats spaghetti? ' + snacks.includes('spaghetti'));
+//     if (snacksAns.toLowerCase() === 'no') {
+//         alert('Correct, I do not care for spaghetti. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. But if I were in Italy I would totally eat pasta.');
+//     } else {
+//         alert('No spaghetti for me. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. But if I were in Italy I would totally eat pasta.');
+//     }
+// };
+
+//Question 6 
 
 // Q6. guessing game that takes numeric value, maximum of four attempts
 
+for (let attempts = 1; attempts < 5; attempts++) {
+    const milesAns = parseInt( prompt('How many miles did I ride yesterday?'));
+    console.log('miles answer was:' + milesAns);
+    if (milesAns === 11) {
+        alert('Good guess! That is correct.');
+        //score++ Add to each of your true answers
+        break;
+    } else if (milesAns <= 10) {
+        alert('Try again! I logged a few more miles.');
+    } else {
+        alert('Not quite that many. Try again.');
+    }
+}
+alert('Maximum tries reached.');
 // let numAttemptsMiles = 1; // initializer, number of attempts
 // const miles = 11; // miles ridden
 // let milesAns = prompt('How many miles did I ride yesterday?'); // user input
