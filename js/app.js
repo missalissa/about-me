529b51e0224dfa7aefc73b2941aa4f196d35f02d
missalissa <alissa.beddow@gmail.com>
2017-10-28 15:49:41 -0700

    finished up Q7, added score tally, and refined prompts

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ alert('Hello, ' + username + '! ' + 'Let\'s get started. The first five question
 
 let score = 0;
 
+// Questions one through 5 are yes / no
 // Q1. Siblings (true)
 
 runSister();
@@ -81,56 +82,56 @@ runSnacks();
 function runSnacks () {
     const snacks = ['salami', 'cheese', 'gummy bears'];
     const snacksAns = prompt('Do I eat spaghetti?');
-    console.log('eats spaghetti? ' + snacks.includes('spaghetti'));
+    console.log('do I eat spaghetti? ' + snacks.includes('spaghetti'));
+    console.log('user replied: ' + snacksAns);
     if (snacksAns.toLowerCase() === 'no' || snacksAns.toLowerCase() === 'n') {
-        alert('Correct, I do not care for spaghetti. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. Unless I were in Italy.');
+        alert('Correct, I do not care for spaghetti. I worked in an Italian restaurant and have eaten enough for one lifetime.');
         score++;
     } else {
-        alert('No spaghetti for me. I worked in an Italian restaurant and have eaten enough pasta for one lifetime. Unless I were in Italy.');
+        alert('No spaghetti for me. I worked in an Italian restaurant and have eaten enough for one lifetime.');
     }
 };
 
-// Q6. guessing game that takes numeric value, maximum of four attempts
+// Q6. Guessing game with numeric value and max four attempts
 
-// runMiles ();
-// function runMiles () {
-//     for (let attempts = 1; attempts < 5; attempts++) {
-//         const milesAns = parseInt( prompt('How many miles did I ride yesterday?'));
-//         console.log('miles answer was:' + milesAns);
-//         if (milesAns === 11) {
-//             alert('Good guess! That is correct.');
-//             //score++ Add to each of your true answers
-//             break;
-//         } else if (milesAns <= 10) {
-//             alert('Try again! I logged a few more miles.');
-//         } else {
-//             alert('Not quite that many. Try again.');
-//         }
-//     }
-//     alert('Maximum tries reached.');
-// };
+alert('Okay, ' + username + ', now for a different kind of quesiton. The answer to this next one will be a number between 1 and 30, and you\'ll have four tries to get it right.');
 
+runMiles ();
+function runMiles () {
+    for (let attempts = 1; attempts < 5; attempts++) {
+        const milesAns = parseInt(prompt('Most days, I ride my bike to school. How many miles is it round trip?'));
+        console.log('user replied: ' + milesAns);
+        if (milesAns === 11) {
+            alert('Good guess! That is correct.');
+            score++;
+            break;
+        } else if (milesAns <= 10) {
+            alert('More than that. You have ' + (4 - attempts) + ' left.');
+        } else {
+            alert('Not quite that many. You have ' + (4 - attempts) + ' left.');
+        }
+    }
+};
 
-// Q7. guessing game with multiple possible correct answers; maximum of six attempts
+// Q7. Guessing game with multiple possible correct answers and max six attempts
 
-// runCheese();
+alert('Final quesiton, ' + username + '. There are lots of possible answers, and you\'ll have six tries this time. Good luck!');
 
-// function runCheese () {
-//     for(let cheeseAttempt = 1; cheeseAttempt < 7; cheeseAttempt++) {
-//         const cheese = ['chevre', 'parmesan', 'blue', 'brie']; // my favorite snacks
-//         let cheeseAns = prompt('Can you guess one of my favorite cheese?');
-//         console.log('what is my favorite cheese answer:' + cheeseAns);
-//         if (cheese.includes(cheeseAns)){
-//             alert('Yep! You guessed it, that is one of my favorite cheeses.');
-//             //score++ 
-//             break;
-//         }else {
-//             alert('Incorrect' + cheeseAttempt + ' guess out of six have been used');
-//         }
-//     }
-// };
+runCheese();
 
-// =======
-// if (cheeseAns === cheese.indexOf()) {
-//     alert('Well done! You are correct.');
-// };
+function runCheese () {
+    for(let cheeseAttempts = 1; cheeseAttempts < 7; cheeseAttempts++) {
+        const cheese = ['chevre', 'parmesan', 'blue', 'brie']; // my favorite cheese
+        const cheeseAns = prompt('Can you guess one of my favorite kinds of cheese?').toLowerCase();
+        console.log('what is my favorite cheese answer:' + cheese.indexOf());
+        console.log('user replied: ' + cheeseAns);
+        if (cheese.includes(cheeseAns)){
+            alert('Yep, you guessed it! My favorites are ' + cheese[0] + ', ' + cheese[1] + ', ' + cheese [2] + ', and ' +  cheese [3] + '.');
+            score++;
+            break;
+        }else {
+            alert('Not so. You have ' + (6 - cheeseAttempts) + ' tries left!');
+        }
+    }
+};
+alert('Thanks for playing, ' + username + '. You got ' + score + ' out of 7 questions correct!');
